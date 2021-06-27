4b5831a4aa669e6455a5243425f5ac6a2f42acf4
harikrish08 <86550242+harikrish08@users.noreply.github.com>
2021-06-27 19:56:43 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+
+function Header()
+{
+  return  (
+    <header>
+      <h1>ShapeAI Bootcamp</h1>
+      </header>
+  );
+}
+export default Header;
